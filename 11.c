d9a24cae7f92f0106f64dc608acf3fba73a41459
a-delusion <durkaclubomir2@gmail.com>
2025-01-15 12:23:37 +0100

    11

---
+++ 11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int maxArea(int* height, int heightSize) {
+    int max = 0;
+    for(int i = 0; i < heightSize; i++){
+        
+    }
+}
+
+int main(void){
+    int height[9] = {1,8,6,2,5,4,8,3,7};
+    printf("%d\n", maxArea(height, 9));
+    return 0;
+}
